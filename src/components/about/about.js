6c191bcfa162154771572b7bb911a5ded2e4a25f
bhowe111 <brendan.howe1@gmail.com>
2021-03-29 21:04:40 -0700

    updated images, info up to resume

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,44 +1,60 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 export default class About extends Component {
   render() {
     let resumeData = this.props.resumeData;
     return (
       <section id="about">
-         <div className="row">
+        <div className="row">
+          <div className="three columns">
+            <img
+              className="profile-pic"
+              src="images/bhowe-studio.jpeg"
+              alt=""
+            />
+          </div>
 
-            <div className="three columns">
+          <div className="nine columns main-col">
+            <h2>About Me</h2>
+            <p>
+              Hello there! I'm a full-stack software engineer experienced in{" "}
+              <span>
+                gathering, analyzing and compiling information from disparate
+                resources into engaging products.
+              </span>{" "}
+              My background in the service industry, education, journalism, and
+              radio informs my problem-solving approach of doing more with less.
+            </p>
 
-               <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
+            <p>
+              I am most interested in working with organizations actively
+              involved in projects to make societies sustainable and equitable.
+            </p>
 
+            <p>
+              I write, conduct, record, and produce interviews with innovators
+              in business technology and the arts. I also play folk rock and go
+              bouldering.
+            </p>
+            <br />
+
+            <div className="row">
+              <div className="columns contact-details">
+                <h2>Contact Details</h2>
+                <p className="address">
+                  <span>{resumeData.name}</span>
+                  <br></br>
+                  <span>{resumeData.address}</span>
+                  <br></br>
+                  <span>
+                    <a href="mailto:brendan.howe1@gmail.com">
+                      {resumeData.email}
+                    </a>
+                  </span>
+                </p>
+              </div>
             </div>
-
-            <div className="nine columns main-col">
-
-               <h2>About Me</h2>
-               <p>
-               {
-                 resumeData.aboutme
-               }
-               </p>
-
-               <div className="row">
-
-                  <div className="columns contact-details">
-
-                  <h2>Contact Details</h2>
-                  <p className="address">
-       						<span>{resumeData.name}</span>
-                     <br></br>
-       						   <span>
-                     {resumeData.address}
-                    </span>
-                    <br></br>
-                    <span>{resumeData.website}</span>
-       					   </p>
-                  </div>
-               </div>
-            </div>
-         </div>
+          </div>
+        </div>
       </section>
     );
   }
--- src/resumeData.js
+++ src/resumeData.js
@@ -19,39 +19,40 @@ let resumeData = {
     },
   ],
   aboutme:
-    "I have finished my B.E. in Computer Science and have been working in industry for past few years.",
-  address: "India",
-  website: "https://www.codeclassifiers.com",
+    "Hello there! I'm a full-stack software engineer experienced in gathering, analyzing and compiling information from disparate resources into engaging products. My background in the service industry, education, journalism, and radio informs my problem-solving approach of doing more with less. I am most interested in working with organizations actively involved in projects to make societies sustainable and equitable. I also write, conduct, record, and produce interviews with innovators in business technology and the arts. I often play rock and blues and climb large boulders.",
+  address: "Seattle, WA",
+  website: "brendanhowe.dev",
+  email: "brendan.howe1@gmail.com",
   education: [
     {
-      UniversityName: "Mumbai University",
-      specialization: "Some specialization",
-      MonthOfPassing: "Jan",
-      YearOfPassing: "2018",
-      Achievements: "Some Achievements",
+      UniversityName: "General Assembly",
+      specialization: "Software Engineering Immersive Graduate",
+      datesAttended: "Sep - Dec 2020",
+      Achievements:
+        "Full-time, 12-week, 480+ hour immersive coding boot camp graduate. Built a strong foundation in web development languages and principles. Worked with a team of engineers and UX/UI designers to win a 36-hour in-house hackathon.",
     },
     {
-      UniversityName: "Some University",
-      specialization: "Some specialization",
-      MonthOfPassing: "Jan",
-      YearOfPassing: "2018",
-      Achievements: "Some Achievements",
+      UniversityName: "University of California, Santa Cruz",
+      specialization: "BA American Studies",
+      datesAttended: "Aug 2009 - Jun 2013",
+      Achievements:
+        "Thesis on Bruce Springsteen's music in American  culture. Graduated with honors in the major.",
     },
   ],
   work: [
     {
-      CompanyName: "Some Company",
-      specialization: "Some specialization",
-      MonthOfLeaving: "Jan",
-      YearOfLeaving: "2018",
-      Achievements: "Some Achievements",
+      CompanyName: "Brendan Howe Communications",
+      specialization: "Writer, Editor, Interviewer",
+      DatesWorked: "Jan 2016 - Present",
+      Achievements:
+        "Plan projects and research extensively prior to interviews. Write and conduct interviews with innovative sources in the arts, business and technology for magazine and blog publication. Utilize exceptional writing, editing and proofreading skills to produce engaging and error-free content.",
     },
     {
-      CompanyName: "Some Company",
-      specialization: "Some specialization",
-      MonthOfLeaving: "Jan",
-      YearOfLeaving: "2018",
-      Achievements: "Some Achievements",
+      CompanyName: "Restaurant Industry",
+      specialization: "Lead Server",
+      DatesWorked: "Jun 2013 - Mar 2020",
+      Achievements:
+        "Maintain calm efficiency under sustained, intense pressure. Manage daily finances and employee schedule changes.",
     },
   ],
   skillsDescription: "Your skills here",
